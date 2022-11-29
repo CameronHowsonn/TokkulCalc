2f8ecf2056d3cb31f85537253ed7cc1ecd871a77
Cameron Howson <cameronhowson@Camerons-MacBook-Pro.local>
2022-11-29 11:58:14 +0000

    add filtering

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,18 @@ function App() {
   const [tokkul, setTokkul] = useState(0);
   const [type, setType] = useState("ores");
   const [sort, setSort] = useState(false);
+  const [hideItems, setHideItems] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const tokkul = e.target.tokkul.value;
     const type = e.target.type.value;
     const sort = e.target.sort.checked;
+    const hideItems = e.target.hideItems.checked;
     setTokkul(tokkul);
     setType(type);
     setSort(sort);
+    setHideItems(hideItems);
   };
 
   const getData = async () => {
@@ -26,54 +29,80 @@ function App() {
           return res.json();
         })
         .then((data) => {
+          if (sort && hideItems) {
+            setItems(
+              data
+                .sort((a, b) => parseInt(a.totalPrice) - parseInt(b.totalPrice))
+                .filter((item) => item.amount > 0)
+            );
+            return;
+          }
           if (sort) {
             setItems(
-              data.sort((a, b) => {
-                return parseInt(a.totalPrice) - parseInt(b.totalPrice);
-              })
+              data.sort(
+                (a, b) => parseInt(a.totalPrice) - parseInt(b.totalPrice)
+              )
             );
-          } else {
-            setItems(data);
+            return;
           }
+          if (hideItems) {
+            setItems(data.filter((item) => item.amount > 0));
+            return;
+          }
+          setItems(data);
         });
     }
   };
 
   useEffect(() => {
     getData();
-  }, [tokkul, type, sort]);
+  }, [tokkul, type, sort, hideItems]);
 
   return (
     <div className="App">
       <h1>Tokkul Calculator</h1>
       <div className="form-container">
         <form onSubmit={handleSubmit}>
-          <label htmlFor="tokkul">Tokkul</label>
-          <select name="type" id="type">
-            <option value="ores" defaultValue={true}>
-              Ores
-            </option>
-            <option value="equipment">Equipment</option>
-            <option value="runes">Runes</option>
-          </select>
-          <input
-            type="number"
-            name="tokkul"
-            id="tokkul"
-            defalutvalue={tokkul}
-          />
-          <button type="submit">Submit</button>
-          <label>Sort by total price?</label>
-          <input type="checkbox" name="sort" id="sort" defaultChecked={sort} />
+          <div className="form-group input">
+            <label htmlFor="tokkul">Tokkul</label>
+            <input
+              type="number"
+              name="tokkul"
+              id="tokkul"
+              defalutvalue={tokkul}
+            />
+            <select name="type" id="type">
+              <option value="ores" defaultValue={true}>
+                Ores
+              </option>
+              <option value="equipment">Equipment</option>
+              <option value="runes">Runes</option>
+            </select>
+          </div>
+          <div className="form-group bottom">
+            <label>Sort by total price?</label>
+            <input
+              type="checkbox"
+              name="sort"
+              id="sort"
+              defaultChecked={sort}
+            />
+            <label>Hide items you can't afford?</label>
+            <input
+              type="checkbox"
+              name="hideItems"
+              id="hideItems"
+              defaultChecked={hideItems}
+            />
+            <button type="submit">Submit</button>
+          </div>
         </form>
       </div>
-      <div className="results-container">
-        <ul>
-          {items.map((item) => {
-            return <ItemCard item={item} key={item.id} />;
-          })}
-        </ul>
-      </div>
+      <ul className="results-container">
+        {items.map((item) => {
+          return <ItemCard item={item} key={item.id} />;
+        })}
+      </ul>
     </div>
   );
 }
